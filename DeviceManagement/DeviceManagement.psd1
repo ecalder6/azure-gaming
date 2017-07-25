9312862f701ddb4e4f9e03f46b503d3a298a3f60
Eric Calder <ecalder6@jhu.edu>
2017-07-25 10:08:08 -0700

    Drivers

---
+++ DeviceManagement/DeviceManagement.psd1
@@ -0,0 +1,14 @@
+﻿@{
+	RootModule = 'DeviceManagement.dll'
+	ModuleVersion = '1.0.3'
+	GUID = 'c500ec28-1f9c-47b0-a623-0173a0b45185'
+	Author = 'Raimund Andree'
+	CompanyName = 'Raimund Andree'
+	Copyright = '2015'
+	Description = 'Windows PowerShell Module for managing devices'
+	PowerShellVersion = '2.0'
+	DotNetFrameworkVersion = '3.5'
+	FormatsToProcess = @('DeviceManagement.format.ps1xml')
+	ModuleList = @('DeviceManagement.dll')
+	FileList = @('DeviceManagement.dll', 'DeviceManagementLib.dll', 'DeviceManagement.psd1')
+}
--- setup.ps1
+++ setup.ps1
@@ -1,33 +1,79 @@
-Write-Host "Run Windows Update (from https://gallery.technet.microsoft.com/scriptcenter/Execute-Windows-Update-fc6acb16)"
-# Invoke-Expression .\PS_WinUpdate.ps1
 
-Write-Host "Enable ICMP Ping in Firewall."
-Set-NetFirewallRule -DisplayName "File and Printer Sharing (Echo Request - ICMPv4-In)" -Enabled True
+function Update-Windows {
+    # Source: https://gallery.technet.microsoft.com/scriptcenter/Execute-Windows-Update-fc6acb16
+    Write-Host "Running Windows Update"
+    Invoke-Expression .\PS_WinUpdate.ps1
+}
 
-Write-Host "Disable Windows Defender real-time protection."
-Set-MpPreference -DisableRealtimeMonitoring $true
+function Update-Firewall {
+    Write-Host "Enable ICMP Ping in Firewall."
+    Set-NetFirewallRule -DisplayName "File and Printer Sharing (Echo Request - ICMPv4-In)" -Enabled True
+}
 
-Write-Host "Disable unnecessary scheduled tasks"
-Disable-ScheduledTask -TaskName 'ScheduledDefrag' -TaskPath '\Microsoft\Windows\Defrag'
-Disable-ScheduledTask -TaskName 'ProactiveScan' -TaskPath '\Microsoft\Windows\Chkdsk'
-Disable-ScheduledTask -TaskName 'Scheduled' -TaskPath '\Microsoft\Windows\Diagnosis'
-Disable-ScheduledTask -TaskName 'SilentCleanup' -TaskPath '\Microsoft\Windows\DiskCleanup'
-Disable-ScheduledTask -TaskName 'WinSAT' -TaskPath '\Microsoft\Windows\Maintenance'
-Disable-ScheduledTask -TaskName 'Windows Defender Cache Maintenance' -TaskPath '\Microsoft\Windows\Windows Defender'
-Disable-ScheduledTask -TaskName 'Windows Defender Cleanup' -TaskPath '\Microsoft\Windows\Windows Defender'
-Disable-ScheduledTask -TaskName 'Windows Defender Scheduled Scan' -TaskPath '\Microsoft\Windows\Windows Defender'
-Disable-ScheduledTask -TaskName 'Windows Defender Verification' -TaskPath '\Microsoft\Windows\Windows Defender'
+function Disable-Defender {
+    Write-Host "Disable Windows Defender real-time protection."
+    Set-MpPreference -DisableRealtimeMonitoring $true
+}
 
-Write-Host "Adjust performance options in registry"
-Set-ItemProperty -Path "HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\VisualEffects" -Name "VisualFXSetting" -Value 2
+function Disable-ScheduledTasks {
+    Write-Host "Disable unnecessary scheduled tasks"
+    Disable-ScheduledTask -TaskName 'ScheduledDefrag' -TaskPath '\Microsoft\Windows\Defrag'
+    Disable-ScheduledTask -TaskName 'ProactiveScan' -TaskPath '\Microsoft\Windows\Chkdsk'
+    Disable-ScheduledTask -TaskName 'Scheduled' -TaskPath '\Microsoft\Windows\Diagnosis'
+    Disable-ScheduledTask -TaskName 'SilentCleanup' -TaskPath '\Microsoft\Windows\DiskCleanup'
+    Disable-ScheduledTask -TaskName 'WinSAT' -TaskPath '\Microsoft\Windows\Maintenance'
+    Disable-ScheduledTask -TaskName 'Windows Defender Cache Maintenance' -TaskPath '\Microsoft\Windows\Windows Defender'
+    Disable-ScheduledTask -TaskName 'Windows Defender Cleanup' -TaskPath '\Microsoft\Windows\Windows Defender'
+    Disable-ScheduledTask -TaskName 'Windows Defender Scheduled Scan' -TaskPath '\Microsoft\Windows\Windows Defender'
+    Disable-ScheduledTask -TaskName 'Windows Defender Verification' -TaskPath '\Microsoft\Windows\Windows Defender'
+}
 
+function Edit-VisualEffectsRegistry {
+    Write-Host "Adjust performance options in registry"
+    Set-ItemProperty -Path "HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\VisualEffects" -Name "VisualFXSetting" -Value 2
+}
 
-Write-Host "Installing Nvidia Driver (modified version of https://github.com/lord-carlos/nvidia-update"
-$r = Invoke-WebRequest -Uri 'https://www.nvidia.com/Download/processFind.aspx?psid=75&pfid=783&osid=74&lid=1&whql=&lang=en-us&ctk=16' -Method GET
+function Install-NvidiaDriver {
+    # Modified from source: https://github.com/lord-carlos/nvidia-update
+    Write-Host "Installing Nvidia Driver"
+    $r = Invoke-WebRequest -Uri 'https://www.nvidia.com/Download/processFind.aspx?psid=75&pfid=783&osid=74&lid=1&whql=&lang=en-us&ctk=16' -Method GET
 
-$version = $r.parsedhtml.GetElementsByClassName("gridItem")[2].innerText
-$url = "http://us.download.nvidia.com/Windows/Quadro_Certified/$version/$version-tesla-desktop-winserver2016-international-whql.exe"
-$driver_file = ".\$version-driver.exe"
+    $version = $r.parsedhtml.GetElementsByClassName("gridItem")[2].innerText
+    $url = "http://us.download.nvidia.com/Windows/Quadro_Certified/$version/$version-tesla-desktop-winserver2016-international-whql.exe"
+    $driver_file = ".\$version-driver.exe"
 
-Write-Host "Downloading Nvidia M60 driver from URL + $url"
-(New-Object System.Net.WebClient).DownloadFile($url, $driver_file)
+    Write-Host "Downloading Nvidia M60 driver from URL + $url"
+    (New-Object System.Net.WebClient).DownloadFile($url, $driver_file)
+
+    Write-Host "Extracting Nvidia M60 driver from file $driver_file"
+    # Start-Process -FilePath ".\$driver_file" -ArgumentList "-s", "-noreboot" -Wait
+
+    $setup_file = "C:\NVIDIA\DisplayDriver\$version\Win10_64\International\setup.exe"
+    Write-Host "Installing Nvidia M60 driver from file $setup_file"
+    # Start-Process -FilePath $setup_file -ArgumentList "-s", "-noreboot", "-noeula" -Wait
+
+    Write-Host "Cleaning up driver files"
+    Remove-Item -Path $driver_file -Confirm:$false
+    Remove-Item "C:\NVIDIA\DisplayDriver\$version\*" -Confirm:$false -Recurse
+}
+
+function Disable-Devices {
+    # Source: https://gallery.technet.microsoft.com/PowerShell-Device-60d73bb0
+    Write-Host "Disabling Hyper-V Video"
+    Import-Module .\DeviceManagement\DeviceManagement.psd1
+    Get-Device | Where-Object -Property Name -Like "Microsoft Hyper-V Video" | Disable-Device -Confirm:$false
+}
+
+function main {
+    # TODO: UNCOMMENT commands
+
+    Update-Windows
+    Update-Firewall
+    Disable-Defender
+    Disable-ScheduledTasks
+    Edit-VisualEffectsRegistry
+    Install-NvidiaDriver
+    Disable-Devices
+}
+
+main
