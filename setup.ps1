2343cb0818611c3af465171fde70edb77a669a15
ecalder6 <ecalder6@jhu.edu>
2017-07-25 22:59:40 -0700

    Disable IE security

--- setup.ps1
+++ setup.ps1
@@ -1,3 +1,13 @@
+function Disable-InternetExplorerESC {
+    # From https://stackoverflow.com/questions/9368305/disable-ie-security-on-windows-server-via-powershell
+    $AdminKey = "HKLM:\SOFTWARE\Microsoft\Active Setup\Installed Components\{A509B1A7-37EF-4b3f-8CFC-4F3A74704073}"
+    $UserKey = "HKLM:\SOFTWARE\Microsoft\Active Setup\Installed Components\{A509B1A8-37EF-4b3f-8CFC-4F3A74704073}"
+    Set-ItemProperty -Path $AdminKey -Name "IsInstalled" -Value 0 -Force
+    Set-ItemProperty -Path $UserKey -Name "IsInstalled" -Value 0 -Force
+    Stop-Process -Name Explorer -Force
+    Write-Host "IE Enhanced Security Configuration (ESC) has been disabled." -ForegroundColor Green
+}
+
 function Update-Windows {
     $url = "https://gallery.technet.microsoft.com/scriptcenter/Execute-Windows-Update-fc6acb16/file/144365/1/PS_WinUpdate.zip"
     $compressed_file = "PS_WinUpdate.zip"
@@ -154,6 +164,7 @@ function main {
         [switch]$windows_update = $false
     )
 
+    Disable-InternetExplorerESC
     if ($windows_update) {
         Update-Windows
     }
