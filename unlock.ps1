81c825c963dd611241837abe138cfccd8c00205f
ecalder6 <t-ercal@microsoft.com>
2017-07-27 18:58:31 -0700

    Fixed unlock powershell script

--- unlock.ps1
+++ unlock.ps1
@@ -1,3 +1,3 @@
 $username = $env:USERNAME
 $session = ((quser /server:$server | Where-Object { $_ -match $username }) -split ' +')[2]
-& "$session /dest:console"
+C:\Windows\System32\tscon.exe $session /dest:console
