0c5c7f5ff0b249da48edf04f9e92262c35e827d0
ecalder6 <ecalder6@jhu.edu>
2017-07-29 20:23:00 -0700

    testing up to virtual audio

--- setup.ps1
+++ setup.ps1
@@ -39,6 +39,6 @@ Install-NSSM
 Enable-Audio
 Install-VirtualAudio
 
-Install-NvidiaDriver $manual_install
-Set-ScheduleWorkflow $steam_username $steam_password $admin_username $admin_password $manual_install
-Restart-Computer
+# Install-NvidiaDriver $manual_install
+# Set-ScheduleWorkflow $steam_username $steam_password $admin_username $admin_password $manual_install
+# Restart-Computer
