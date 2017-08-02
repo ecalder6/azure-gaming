91d0002010a1a4fc349bca0038d37b8fb74854d9
ecalder6 <ecalder6@jhu.edu>
2017-08-01 21:54:42 -0700

    Remove writing out steam password (used for debugging)

--- steamLogin.ps1
+++ steamLogin.ps1
@@ -7,5 +7,4 @@ $steam = "C:\Program Files (x86)\Steam\Steam.exe"
 $steam_password = Get-Content $password_file | ConvertTo-SecureString
 $BSTR = [System.Runtime.InteropServices.Marshal]::SecureStringToBSTR($steam_password)            
 $PlainPassword = [System.Runtime.InteropServices.Marshal]::PtrToStringAuto($BSTR)
-Write-Host "$PlainPassword"
 Start-Process -FilePath $steam -ArgumentList "-login $steam_username $PlainPassword", "-silent"
