e402eba5dd3025a1d8447745a32357434817bdfd
ecalder6 <ecalder6@jhu.edu>
2017-08-01 21:20:43 -0700

    Decrypt password to pass to steam client

--- steamLogin.ps1
+++ steamLogin.ps1
@@ -3,7 +3,9 @@ param (
     [string]$password_file = ""
 )
 
-
 $steam = "C:\Program Files (x86)\Steam\Steam.exe"
 $steam_password = Get-Content $password_file | ConvertTo-SecureString
-Start-Process -FilePath $steam -ArgumentList "-login $steam_username $steam_password", "-silent" -Wait
+$BSTR = [System.Runtime.InteropServices.Marshal]::SecureStringToBSTR($steam_password)            
+$PlainPassword = [System.Runtime.InteropServices.Marshal]::PtrToStringAuto($BSTR)
+Write-Host "$PlainPassword"
+Start-Process -FilePath $steam -ArgumentList "-login $steam_username $PlainPassword", "-silent"
