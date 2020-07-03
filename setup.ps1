d1c74d6467776bf4ea710ca47cab25f7c7300411
Amine <amine.98@hotmail.fr>
2020-07-03 22:53:22 +0200

    Install ZeroTier only when networkID is present
    Cherry-picked from #39
    
    Co-Authored-By: Andrei Radulescu <andi.radulescu@gmail.com>

--- setup.ps1
+++ setup.ps1
@@ -32,9 +32,11 @@ if ($manual_install) {
 }
 Add-DisconnectShortcut
 
-Install-Chocolatey
-Install-VPN
-Join-Network $network
+if ($network) {
+    Install-Chocolatey
+    Install-VPN
+    Join-Network $network
+}
 
 Disable-Devices
 Disable-TCC
